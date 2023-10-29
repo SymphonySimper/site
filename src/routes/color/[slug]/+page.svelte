8f607c691e7bde2a7739be332c55e342520d3aaa
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2023-10-29 12:47:16 +0530

    add color route

---
+++ src/routes/color/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	export let data;
+</script>
+
+{#if data.hex}
+	<div class="h-screen" style={`background: #${data.hex}`} />
+{:else}
+	<h1 class="flex h-screen items-center justify-center font-mono">Not a proper hex</h1>
+{/if}
