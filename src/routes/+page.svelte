94cdd8e08fb2caec50e8848cc616170fd7869287
SymphonySimper <50240805+SymphonySimper@users.noreply.github.com>
2025-09-01 19:34:21 +0530

    NUKE!

--- src/routes/+page.svelte
+++
@@ -1,1 +0,0 @@
-<h1 class="flex h-screen items-center justify-center font-mono">Welcome!</h1>
--- src/routes/color/[slug]/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	export let data;
-</script>
-
-{#if data.hex}
-	<div class="h-screen" style={`background: #${data.hex}`} />
-{:else}
-	<h1 class="flex h-screen items-center justify-center font-mono">Not a proper hex</h1>
-{/if}
